49045972e189f75a8fb00d1b380f336c4ab1da6e
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-05-23 15:10:42 -0400

    Fix index.html and style.css, change index.js to add Event Listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e 
+//TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
+
+document.addEventListener('DOMContentLoaded', () => {
+    const areaTexto = document.querySelector('textarea[name="user-input"]');
+    const btnResetear = document.getElementById('reset-button');
+
+    btnResetear.addEventListener('click', () => {
+        areaTexto.value = '';
+    });
+});
