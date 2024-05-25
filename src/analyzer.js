07068353d5423f6e508805318661270011260cd3
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-05-24 21:48:10 -0400

    Update index.js & analyzer.js for getCharacterCount, fix style.css to align all elements, change font of header and footer

--- src/analyzer.js
+++ src/analyzer.js
@@ -5,7 +5,9 @@ const analyzer = {
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    return text.length;
+    let characterCount = 0;
+    characterCount = text.length;
+    return characterCount;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
 
+/* 
+Asegurar que el DOM esté completamente cargado antes de que se ejecuten los scripts
+Declaraciones para seleccionar <textarea> basado en su name y <button> por su id
+*/
 document.addEventListener('DOMContentLoaded', () => {
     const areaTexto = document.querySelector('textarea[name="user-input"]');
     const btnResetear = document.getElementById('reset-button');
+    const verCharacterCount = document.querySelector('[data-testid="character-count"]');
 
+//Event listener para borrar el contenido del textarea al hacer click al botón
     btnResetear.addEventListener('click', () => {
         areaTexto.value = '';
+        verCharacterCount.textContent = 'Caracteres: 0';
+    });
+
+ //Event listener para escuchar contenido ingresado en el textarea
+    areaTexto.addEventListener('input', (event) => {
+        //Para obtener el contenido actual del <textarea>
+        const contenido = event.target.value;
+        //Para imprimir el contenido actual en la consola
+        console.log(`Texto ingresado: ${contenido}`);
+        //Aquí invocar métodos del objeto analyzer!
+        const characterCount = analyzer.getCharacterCount(contenido);
+        verCharacterCount.textContent = `Caracteres: ${characterCount}`;
     });
 });
