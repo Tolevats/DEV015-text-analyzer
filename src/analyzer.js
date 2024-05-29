5f1cb680ea82a831f2190bbfa1f2a819d32b8ea9
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-05-28 23:47:58 -0400

    fix getCharacterCountExcludingSpaces, add function getNumberCount

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 //Declaración del objeto `analyzer`
 const analyzer = {
   //Lo siguiente son métodos que toman un string `text` como parámetro según cada función (clave + valor) 
@@ -13,35 +15,66 @@ const analyzer = {
     }
     return count;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
     /* // v1 Probando con variables:  
     let characterCount = 0;
-    characterCount = text.length; 
+    characterCount = text.length;
+    return characterCount; 
     // v2 Más simple: 
-    let characterCount = text.length; */
+    let characterCount = text.length; 
+    return characterCount*/
     // v3 Muy simple:
     return text.length;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    //Usando bucle `for`
+    //Usando `for` con array de signos de puntuación para excluirlos del conteo de caracteres
     let count = 0;
+    const punctuation = ['.', ',', ':', ';', '‘', '’', '“', '”', '«', '»','¡', '!', '¿', '?', '-', '_', '[', ']', '{', '}', '(', ')'];
+
     for (let i = 0; i < text.length; i++) {
-      if (text[i] !== ' ') {
+      if (text[i] !== ' ' && !punctuation.includes(text[i])) {
         count++;
       }
     }
     return count;
   },
+
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
 
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    
+    //Usando `for`
+    /* v1:     
+    const parts = text.split(' ');
+    let count = 0;
+    for (let i = 0; i < parts.length; i++) {
+      if (!isNaN(Number(parts[i])) && parts[i].trim() !== '') {
+        count++;
+      }
+    }
+    return count;  */
+    //v2:
+    const parts = text.split(' ');
+    let count = 0;
+
+    for (let i = 0; i < parts.length; i++) {
+      const part = parts[i].trim();
+
+      if (part !== '' && !isNaN(Number(part))) {
+        count++;
+      }
+    }
+
+    return count;
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
     
--- src/index.js
+++ src/index.js
@@ -7,36 +7,38 @@ Asegurar que el DOM esté completamente cargado antes de que se ejecuten los scr
 Declaraciones para seleccionar <textarea> por su atributo con valor, <button> por su id y métricas por su atributo con valor.
 */
 document.addEventListener('DOMContentLoaded', () => {
-    const areaTexto = document.querySelector('textarea[name="user-input"]');
-    const btnResetear = document.getElementById('reset-button');
-    const cntdrCaracteres = document.querySelector('[data-testid="character-count"]');
-    const cntdrCaracteresSin = document.querySelector('[data-testid="character-no-spaces-count"]');
-    const cntdrPalabras = document.querySelector('[data-testid="word-count"]');
+  const areaTexto = document.querySelector('textarea[name="user-input"]');
+  const btnResetear = document.getElementById('reset-button');
+  const cntdrCaracteres = document.querySelector('[data-testid="character-count"]');
+  const cntdrCaracteresSin = document.querySelector('[data-testid="character-no-spaces-count"]');
+  const cntdrPalabras = document.querySelector('[data-testid="word-count"]');
+  const cntdrNumeros = document.querySelector('[data-testid="number-count"]');
 
-//Event listener para borrar el contenido del textarea al hacer click al botón
-    btnResetear.addEventListener('click', () => {
-        areaTexto.value = '';
-        cntdrCaracteres.textContent = 'Caracteres: 0';
-        cntdrCaracteresSin.textContent = 'Caracteres Sin Espacio: 0';
-        cntdrPalabras.textContent = 'Palabras: 0';
-    });
+  //Event listener para borrar el contenido del textarea al hacer click al botón
+  btnResetear.addEventListener('click', () => {
+    areaTexto.value = '';
+    cntdrCaracteres.textContent = 'Caracteres: 0';
+    cntdrCaracteresSin.textContent = 'Caracteres Sin Espacio: 0';
+    cntdrPalabras.textContent = 'Palabras: 0';
+    cntdrNumeros.textContent = 'Números: 0';
+  });
 
- //Event listener para escuchar contenido ingresado en el textarea
-    areaTexto.addEventListener('input', (event) => {
-        //Para obtener el contenido actual del <textarea>
-        const contenido = event.target.value;
+  //Event listener para escuchar contenido ingresado en el textarea
+  areaTexto.addEventListener('input', (event) => {
+    //Para obtener el contenido actual del <textarea>
+    const contenido = event.target.value;
 
-        //Para imprimir en la consola
-        console.log(`Texto ingresado: ${contenido}`);
+    //Aquí invocar métodos del objeto analyzer!
+    const verCntdrCaracteres = analyzer.getCharacterCount(contenido);
+    cntdrCaracteres.textContent = `Caracteres: ${verCntdrCaracteres}`;
 
-        //Aquí invocar métodos del objeto analyzer!
-        const verCntdrCaracteres = analyzer.getCharacterCount(contenido);
-        cntdrCaracteres.textContent = `Caracteres: ${verCntdrCaracteres}`;
+    const verCntdrCaracteresSin = analyzer.getCharacterCountExcludingSpaces(contenido);
+    cntdrCaracteresSin.textContent = `Caracteres Sin Espacio: ${verCntdrCaracteresSin}`;
 
-        const verCntdrCaracteresSin = analyzer.getCharacterCountExcludingSpaces(contenido);
-        cntdrCaracteresSin.textContent = `Caracteres Sin Espacio: ${verCntdrCaracteresSin}`;
+    const verCntdrPalabras = analyzer.getWordCount(contenido);
+    cntdrPalabras.textContent = `Palabras: ${verCntdrPalabras}`;
 
-        const verCntdrPalabras = analyzer.getWordCount(contenido);
-        cntdrPalabras.textContent = `Palabras: ${verCntdrPalabras}`;
-    });
+    const verCntdrNumeros = analyzer.getNumberCount(contenido);
+    cntdrNumeros.textContent = `Números: ${verCntdrNumeros}`;
+  });
 });
